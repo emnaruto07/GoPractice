b6a127e763fa72fb333cf08d29d5e0f03d7ea14b
Sc0rpion2 <shazebhussaini1994@gmail.com>
2020-09-30 18:43:54 +0530

    chan

--- urlchecker/main.go
+++ urlchecker/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Test")
+	c := make(chan string)
+	go CheckUrl(c)
+	v := <-c
+	fmt.Println("Value is:", v)
+
+}
+
+//CheckUrl to check url
+func CheckUrl(c chan string) {
+	var withHttp string
+
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		domain := strings.ToLower(sc.Text())
+		//withHttps = "https://" + domain
+		withHttp = "http://" + domain
+
+		resp, err := http.Get(withHttp)
+		if err != nil {
+			continue
+		}
+		fmt.Println(withHttp, resp.StatusCode, http.StatusText(resp.StatusCode))
+	}
+	c <- withHttp
+
 }
