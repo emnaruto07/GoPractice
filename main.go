a2e952ace3897cc6d4076920f8735bf0fb9a2888
emnaruto07 <shazebvps@gmail.com>
2021-05-06 16:14:10 +0530

    #2198 loop

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/anaskhan96/soup"
 )
 
-const url = "https://xkcd.com/"
+const url = "https://xkcd.com/2199"
 
 var comicURL string
 
@@ -27,16 +27,15 @@ func imageDownloader(u string) {
 		os.Mkdir("xkcd", 0755)
 	} else {
 		fmt.Println("[-]Removing old xkcd folder")
-		os.Remove(`xkcd`)
+		os.RemoveAll(`xkcd`)
 		fmt.Println("[+]Now Creating new xkcd Folder!!")
 		os.Mkdir("xkcd", 0755)
 	}
-	//Checking if there is # in the URL
+	//Checking if there is # in the end of the URL
 	if !strings.HasSuffix(u, "#") {
 		for {
 			fmt.Printf("Downloading page from %v...\n", u)
 			resp, err := soup.Get(u)
-
 			if err != nil {
 				panic(err)
 			}
@@ -47,30 +46,28 @@ func imageDownloader(u string) {
 				fmt.Printf("Downloading image from %v...\n", comicURL)
 				resp, err := soup.Get(comicURL)
 				if err != nil {
-					log.Panic(err)
+					fmt.Println("[!!]Invalid URL")
+				} else {
+					//Save the image to ./xkcd
+					os.Chdir("./xkcd")
+					file, err := os.Create(comicURL[28:])
+					if err != nil {
+						log.Fatalln("[-]Check file names")
+					}
+
+					reader := strings.NewReader(resp)
+					_, err = io.Copy(file, reader)
+					if err != nil {
+						log.Fatal(err)
+					}
+					prevLink := html.Find("a", "rel", "prev")
+					u = "http://xkcd.com" + prevLink.Attrs()["href"]
 				}
-
-				os.Chdir("./xkcd")
-				file, err := os.Create(comicURL[28:])
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				reader := strings.NewReader(resp)
-				_, err = io.Copy(file, reader) //here
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				// }
-				prevLink := html.Find("a", "rel", "prev")
-				u = "http://xkcd.com" + prevLink.Attrs()["href"]
-
-				//Save the image to ./xkcd
 
 			}
 
 		}
 
 	}
+
 }
